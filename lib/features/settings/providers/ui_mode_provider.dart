879d3fa0e06d248740d23c70f80688343bd67916
Lazar Dóra Csilla <dora.lazar@mailbox.unideb.hu>
2025-11-25 17:33:44 +0100

    Dependency javitás

--- lib/features/settings/providers/ui_mode_provider.dart
+++ lib/features/settings/providers/ui_mode_provider.dart
@@ -1,8 +1,11 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'ui_mode_provider.g.dart';
 
 enum UiMode { standard, simplified }
+
+const _kUiModeKey = 'ui_mode';
 
 @Riverpod(keepAlive: true)
 class UiModeController extends _$UiModeController {
