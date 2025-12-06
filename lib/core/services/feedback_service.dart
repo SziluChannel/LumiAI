f9712fdf11607575f32e8b7b48678a98fe6910cb
SziluChannel <szaszilumin@gmail.com>
2025-12-06 15:23:24 +0100

    Android visszajelzések és ujjlenyomat használati lehetőségek megjavítása

--- lib/core/services/feedback_service.dart
+++ lib/core/services/feedback_service.dart
@@ -11,17 +11,37 @@ class FeedbackService {
 
   FeedbackService(this._ref);
 
+  /// Checks if haptic feedback is enabled (defaults to true if loading)
+  bool get _isHapticEnabled {
+    final asyncValue = _ref.read(hapticFeedbackControllerProvider);
+    return asyncValue.hasValue ? asyncValue.value! : true;
+  }
+
   /// Provides a light impact vibration for successful actions.
   void triggerSuccessFeedback() {
-    if (_ref.read(hapticFeedbackControllerProvider).value!) {
+    if (_isHapticEnabled) {
       HapticFeedback.lightImpact();
     }
   }
 
   /// Provides a heavy impact vibration for error or failed actions.
   void triggerErrorFeedback() {
-    if (_ref.read(hapticFeedbackControllerProvider).value!) {
+    if (_isHapticEnabled) {
       HapticFeedback.heavyImpact();
+    }
+  }
+
+  /// Provides a medium impact vibration.
+  void triggerMediumFeedback() {
+    if (_isHapticEnabled) {
+      HapticFeedback.mediumImpact();
+    }
+  }
+
+  /// Provides a selection click feedback.
+  void triggerSelectionFeedback() {
+    if (_isHapticEnabled) {
+      HapticFeedback.selectionClick();
     }
   }
 }
